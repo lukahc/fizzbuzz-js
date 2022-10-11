4e97c165d7036b9233dba3c8208e05cc181d0dc7
Luka <luka.cowley@softwire.com>
2022-10-11 17:11:39 +0100

    Second Task

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,10 +6,18 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     for (let i=1; i<101; i++) {
-        console.log(i);
+        if (i%5==0){
+            if (i%3==0){
+                console.log('FizzBuzz')
+            } else {
+                console.log('Buzz')
+            }
+        } else if (i%3==0){
+            console.log('Fizz')
+        } else{
+            console.log(i)
+        }
     }
-
-    
 }
 
 // Now we run the main function...
