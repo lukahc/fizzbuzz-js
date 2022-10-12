ba8fb97b1b79fb71c2d7af8dbbe40bdd7f688231
Luka <luka.cowley@softwire.com>
2022-10-12 17:00:14 +0100

    Stretch 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,43 +1,83 @@
+const prompt = require("prompt-sync")();
+
 // Here, we create our main function.
 function fizzbuzz() {
-  console.log("Welcome to FizzBuzz!");
-  const prompt = require('prompt-sync')();
-  const maxNum = Number(prompt("Please enter maximum number: ")) + 1;
-  for (let i = 1; i < maxNum; i++) {
-    let outputArray = [];
-    if (i % 3 == 0) {
-      outputArray.push("Fizz");
-    }
-    if (i % 5 == 0) {
-      outputArray.push("Buzz");
-    }
-    if (i % 7 == 0) {
-      outputArray.push("Bang");
-    }
-    if (i % 11 == 0) {
-      outputArray = ["Bong"];
-    }
-    if (i % 13 == 0) {
-      for (let j in outputArray) {
-        if (outputArray[j].startsWith("B")) {
-          outputArray.splice(j, 0, "Fezz");
-          break;
+    console.log("Welcome to FizzBuzz!");
+    const maxNum = Number(prompt("Please enter maximum number: ")) + 1;
+    const rules = rulesQuery();
+    loopValues(maxNum, rules);
+}
+
+function rulesQuery() {
+    let rules = {
+        Fizz: 3,
+        Buzz: 5,
+        Bang: 7,
+        Bong: 11,
+        Fezz: 13,
+        Reverse: 17,
+    };
+    while (true) {
+        let custom = prompt(
+            "Would you like to use custom rules? (Y/N) "
+        ).toUpperCase();
+        if (custom == "Y") {
+            rules = customRules(rules);
+            break;
+        } else if (custom == "N") {
+            break;
+        } else {
+            console.log("Invalid input");
         }
-      }
-      if (!outputArray.includes("Fezz")) {
-        outputArray.push("Fezz");
-      }
     }
-    if (i % 17 == 0) {
-      outputArray = outputArray.reverse();
+    return rules;
+}
+
+function customRules(rules) {
+    for (let j in rules) {
+        rules[j] = Number(prompt("Enter number for " + j + ": "));
     }
+    return rules;
+}
+
+function loopValues(maxNum, rules) {
+    for (let i = 1; i < maxNum; i++) {
+        let outputArray = [];
+
+        //Check values
+        let ruleNum = 0;
+        for (let j in rules) {
+            if (i % rules[j] == 0) {
+                if (ruleNum == 3) {
+                    outputArray = ["Bong"];
+                } else if (ruleNum == 4) {
+                    let fezzPos = outputArray.length;
+                    for (let k in outputArray) {
+                        if (outputArray[k].startsWith("B")) {
+                            fezzPos = k;
+                            break;
+                        }
+                    }
+                    outputArray.splice(fezzPos, 0, "Fezz");
+                } else if (ruleNum == 5) {
+                    outputArray = outputArray.reverse();
+                } else {
+                    outputArray.push(j);
+                }
+            }
+            ruleNum += 1;
+        }
+        output(outputArray, i);
+    }
+}
+
+function output(outputArray, currentNum) {
     if (outputArray.length == 0) {
-      console.log(i);
+        console.log(currentNum);
     } else {
-      const outputString = outputArray.join("");
-      console.log(outputString);
+        const outputString = outputArray.join("");
+        console.log(outputString);
     }
-  }
 }
 
 // Now we run the main function...
