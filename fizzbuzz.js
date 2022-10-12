b6e8033dc74dc1a2b7410a53a2090f5a07227765
Luka <luka.cowley@softwire.com>
2022-10-12 11:34:12 +0100

    Third Task

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,42 @@
-
-
 // Here, we create our main function.
 function fizzbuzz() {
-    
-    console.log('Welcome to FizzBuzz!');
+  console.log("Welcome to FizzBuzz!");
 
-    for (let i=1; i<101; i++) {
-        if (i%5==0){
-            if (i%3==0){
-                console.log('FizzBuzz')
-            } else {
-                console.log('Buzz')
-            }
-        } else if (i%3==0){
-            console.log('Fizz')
-        } else{
-            console.log(i)
-        }
+  for (let i = 1; i < 101; i++) {
+    let outputArray = [];
+    if (i % 3 == 0) {
+      outputArray.push("Fizz");
     }
+    if (i % 5 == 0) {
+      outputArray.push("Buzz");
+    }
+    if (i % 7 == 0) {
+      outputArray.push("Bang");
+    }
+    if (i % 11 == 0) {
+      outputArray = ["Bong"];
+    }
+    if (i % 13 == 0) {
+      for (let j in outputArray) {
+        if (j.startsWith("B")) {
+          outputArray.splice(j-1, 0, "Fezz");
+          break;
+        }
+      }
+      if (!outputArray.includes("Fezz")) {
+        outputArray.push("Fezz");
+      }
+    }
+    if (i % 17 == 0) {
+      outputArray = outputArray.reverse();
+    }
+    if (outputArray.length == 0) {
+      console.log(i);
+    } else {
+      const outputString = outputArray.join("");
+      console.log(outputString);
+    }
+  }
 }
 
 // Now we run the main function...
